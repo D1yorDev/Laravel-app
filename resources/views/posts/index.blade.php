f70f249d1b24d0f9050d809ee34b2dd375bc315a
d1yordev <diorkh07@gmail.com>
2024-07-15 01:06:53 +0500

    i learned Pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -39,29 +39,13 @@
                         href="{{ route('posts.show', ['post' => $post->id]) }}">O'qish</a>
                 </div>
                 @endforeach
-
-
                 <div class="col-12">
-                    <nav aria-label="Page navigation">
-                        <ul class="pagination pagination-lg justify-content-center mb-0">
-                            <li class="page-item disabled">
-                                <a class="page-link" href="#" aria-label="Previous">
-                                    <span aria-hidden="true">&laquo;</span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                            </li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Next">
-                                    <span aria-hidden="true">&raquo;</span>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+
+                    {{ $posts->links() }}
+
                 </div>
+
+
             </div>
         </div>
     </div>
